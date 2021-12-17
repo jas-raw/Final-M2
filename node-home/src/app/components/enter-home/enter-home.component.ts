fb22a36a313fb162c485b763559ee6b61b70a98d
danielavargashidalgo@gmail.com <laura.vargas.mt@correo.usa.edu.co>
2021-12-17 13:01:40 -0500

    feat[FMN-5]:front Se añadio pagina principal, login y de  productos disponibles

---
+++ node-home/src/app/components/enter-home/enter-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enter-home',
+  templateUrl: './enter-home.component.html',
+  styleUrls: ['./enter-home.component.scss']
+})
+export class EnterHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ node-home/src/app/components/houses-available/houses-available.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-houses-available',
+  templateUrl: './houses-available.component.html',
+  styleUrls: ['./houses-available.component.scss']
+})
+export class HousesAvailableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
